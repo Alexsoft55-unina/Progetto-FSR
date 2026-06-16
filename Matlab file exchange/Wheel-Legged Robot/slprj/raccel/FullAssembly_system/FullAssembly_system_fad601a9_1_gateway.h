7fabf18a4ccaa9d9592bbafb9dc8b520df5476c3
Alessandro Prisco <alessand.prisco@studenti.unina.it>
2026-06-16 20:15:43 +0200

    Aggiustato nome e aggiunto file matlab con Modello CAD

---
+++ Matlab file exchange/Wheel-Legged Robot/slprj/raccel/FullAssembly_system/FullAssembly_system_fad601a9_1_gateway.h
@@ -0,0 +1,10 @@
+#ifndef __FullAssembly_system_fad601a9_1_gateway_h__
+#define __FullAssembly_system_fad601a9_1_gateway_h__
+#ifdef __cplusplus
+extern "C" {
+#endif
+extern void FullAssembly_system_fad601a9_1_gateway ( void ) ;
+#ifdef __cplusplus
+}
+#endif
+#endif
